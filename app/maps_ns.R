fd18cced0d43d5ace0329b25073c6b7f4751eb01
safiraharjo <19592104+safiraharjo@users.noreply.github.com>
2023-02-16 23:27:51 -0500

    Update maps_ns.R
    still error with color of markers and filter somewhat works but won't clear last selection

--- app/maps_ns.R
+++ app/maps_ns.R
@@ -49,51 +49,18 @@ if (!require("tm")) {
   library(tm)
 }
 
-#Data Prepocessing
-library(readr)
 current_path = rstudioapi::getActiveDocumentContext()$path 
 setwd(dirname(current_path ))
-year_options = c("2016", "2017",
-                 "2018", "2019",
-                 "2020", "2021",
-                 "2022")
-restaurant_inspections <- read_csv("../data/DOHMH_New_York_City_Restaurant_Inspection_Results.csv")
-restaurant_inspections_filtered <- restaurant_inspections[!(is.na(restaurant_inspections$`VIOLATION DESCRIPTION`) | restaurant_inspections$`VIOLATION DESCRIPTION`==""), ]
+source('global.R')
+df_unique
 
-restaurant_inspections_filtered_1 <- restaurant_inspections_filtered[!(is.na(restaurant_inspections_filtered$Latitude) | restaurant_inspections_filtered$Latitude=="" | 
-                                                                         is.na(restaurant_inspections_filtered$Longitude) | restaurant_inspections_filtered$Longitude=="") , ]
-restaurant_inspections_filtered_1$dateandtime <- as.POSIXct(restaurant_inspections_filtered_1$`INSPECTION DATE`, format = "%m/%d/%Y")
-restaurant_inspections_filtered_1$years <- format(restaurant_inspections_filtered_1$dateandtime, format = "%Y")
-restaurant_inspections_filtered_2 <- restaurant_inspections_filtered_1[which(restaurant_inspections_filtered_1$years %in% year_options),]
+#Data Prepocessing
+year_options = c(2016, 2017,
+                 2018, 2019,
+                 2020, 2021,
+                 2022)
 
-
-restaurant_inspections_filtered_3 <- restaurant_inspections_filtered_2[order(restaurant_inspections_filtered_2$DBA, 
-                                                                             as.Date(restaurant_inspections_filtered_2$`INSPECTION DATE`, format = "%m/%d/%Y")),]
-
-restaurant_inspections_filtered_3$criticalviolations <- ifelse(restaurant_inspections_filtered_3$`CRITICAL FLAG` == "Critical", 1, 0)
-restaurant_inspections_filtered_3$noncriticalviolations <- ifelse(restaurant_inspections_filtered_3$`CRITICAL FLAG` == "Not Critical", 1, 0)
-
-
-restaurant_inspections_filtered_3$ACTION <- ifelse(restaurant_inspections_filtered_3$ACTION == "Violations were cited in the following area(s).", 
-                                                   "Violations", ifelse(restaurant_inspections_filtered_3$ACTION == "No violations were recorded at the time of this inspection.",
-                                                                        "No violations", ifelse(restaurant_inspections_filtered_3$ACTION == "Establishment Closed by DOHMH. Violations were cited in the following area(s) and those requiring immediate action were addressed.",
-                                                                                                "Closed", ifelse(restaurant_inspections_filtered_3$ACTION == "Establishment re-closed by DOHMH.", "Re-closed",
-                                                                                                                 "Re-opened")
-                                                                        )
-                                                   ))
-restaurant_inspections_filtered_year <- filter(restaurant_inspections_filtered_3,years %in% year_options)
-restaurant_inspections_filtered_year$months <- format(restaurant_inspections_filtered_year$dateandtime, format = "%m-%Y")
-df1 <- aggregate(cbind(restaurant_inspections_filtered_year$criticalviolations,restaurant_inspections_filtered_year$noncriticalviolations) ~ 
-                   restaurant_inspections_filtered_year$DBA, data = restaurant_inspections_filtered_year, FUN = sum, na.rm = TRUE)
-
-df1$criticalYN <- ifelse(df1$V1 > df1$V2, "Critical", "Non-Critical")
-df1$DBA <- df1$`restaurant_inspections_filtered_year$DBA`
-df1$critical <- df1$V1
-df1$noncritical <- df1$V2
-df1 <-subset(df1, select = -c(V1, V2, `restaurant_inspections_filtered_year$DBA`))
-
-total <- merge(df1, restaurant_inspections_filtered_year,by="DBA")
-total1 <- total %>% distinct(DBA, .keep_all = TRUE)
+df <- filter(df_unique,inspection_year %in% year_options)
 
 # Define UI
 
@@ -108,9 +75,8 @@ ui <- fluidPage(
          box(width = NULL,  
              selectInput(
                inputId = "years",
-               label = "Choose a Year:",
-               choices = year_options,
-               selected = "2019"
+               label = "Inspection year:",
+               choices = year_options
              )
              
          )
@@ -123,8 +89,8 @@ server <- shinyServer(function(input, output) {
   
   #maps
   output$map <- renderLeaflet({
-    data <- total1
-    color <- colorFactor(topo.colors(2), data$criticalYN)
+    data <- df
+    color <- colorFactor(topo.colors(2), data$violation_type)
     leaflet(data) %>%
       setView(lng = -73.98928, lat = 40.75042, zoom = 10) %>%
       addProviderTiles("CartoDB.Positron", options = providerTileOptions(noWrap = TRUE)) %>%
@@ -132,36 +98,33 @@ server <- shinyServer(function(input, output) {
       addLegend(
         "bottomleft", # Legend position
         pal=color, # color palette
-        values=~data$criticalYN, # legend values
+        values=~data$violation_type, # legend values
         opacity = 1,
-        title="Critical Violation"
+        title="Violation Type"
       )
     
   })
   
   observeEvent(input$years, {
-    years_picked <- total1[total1$years == input$years,  c("Latitude", "Longitude")]
+    years_picked <- df[df$inspection_year == input$years, ]
     leafletProxy("map", data = years_picked) %>%
-      clearMarkers() %>%
-      addCircleMarkers(
-        lng=~data$Longitude, # Longitude coordinates
-        lat=~data$Latitude, # Latitude coordinates
-        radius=~ifelse(data$criticalYN == "Critical", 2, 2), # Total count
-        stroke=FALSE, # Circle stroke
-        fillOpacity=1, # Circle Fill Opacity
-        color=~color(data$criticalYN),
-        popup=~paste(
-          "<b>", data$DBA , "</b><br/>",
-          "Violation Status : ", as.character(data$criticalYN), "<br/>",
-          "Borough: ", as.character(data$BORO), "<br/>",
-          "Inspection date: ", as.character(data$dateandtime)
-        )
-        # Popup content
+    addCircleMarkers(
+      lng=~years_picked$longitude, # Longitude coordinates
+      lat=~years_picked$latitude, # Latitude coordinates
+      radius=4,
+      stroke=FALSE, # Circle stroke
+      fillOpacity=0.5, # Circle Fill Opacity
+      # color=~color(years_picked$violation_type),
+      popup=~paste(
+        "<b>", years_picked$restaurant_name , "</b><br/>",
+        "Violation Type : ", as.character(years_picked$violation_type), "<br/>",
+        "Address: ", as.character(years_picked$building), " ", as.character(years_picked$street), "<br/>",
+        "Inspection Date: ", as.character(years_picked$inspection_date), "<br/>",
+        "Violation Description: ", as.character(years_picked$violation_description)
       )
+    )
       })
   })
-
-
 
 # Run the Shiny app
 shinyApp(ui, server)

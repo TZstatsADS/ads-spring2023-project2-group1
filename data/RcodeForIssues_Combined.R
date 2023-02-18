2ad3ba6cedd0f10d3cb4fae64fce5563a609728c
Ash290495 <123602048+Ash290495@users.noreply.github.com>
2023-02-18 17:41:27 -0500

    Final commit for Combined WC
    Final commit for Combined Wordcloud

--- data/RcodeForIssues_Combined.R
+++ data/RcodeForIssues_Combined.R
@@ -10,39 +10,66 @@
 library(shiny)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
 
-    # Application title
-    titlePanel("Major Issues in Restaurants in NYC"),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("output$plot")
-        )
-    )
-)
+#Zara_bar plot
+    ui <- navbarPage(
+      "NYC Restaurant",
+      tabPanel(
+        "Static Plots",
+        sidebarLayout(
+          sidebarPanel(
+            selectInput(inputId = "borough",
+                        label = "Choose a borough:",
+                        choices = c("Manhattan", "Brooklyn",
+                                    "Staten Island", "Queens",
+                                    "Bronx"))
+          ),
+          mainPanel(
+            plotOutput(outputId = "plot")
+          )
+    )))
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
+  library(shiny)
+  
+  df=read.csv("C:/Users/naira_831868r/OneDrive/Desktop/Columbia/Spring '23/Applied Data Science/Assignments/Shiny App/DOHMH_New_York_City_Restaurant_Inspection_Results.csv")
+  head(df)
+  
+  #For wordclouds per violation code
+  df_violation_temp<-df |>
+    distinct(df$VIOLATION.CODE)
+  df_violation_temp
+  
+  freq=table(unlist(df$VIOLATION.CODE)) #Find frequency of violation codes to know what wordclouds make sense
+  freq 
+  
+  library(dplyr)
+  library(wordcloud)
+  
+  library(ggplot2)
+  install.packages("stringr")
+  library("stringr")
+  install.packages("repr")
+  install.packages("magrittr") # package installations are only needed the first time you use it
+  install.packages("dplyr")    # alternative installation of the %>%
+  library(magrittr) # needs to be run every time you start R and want to use %>%
+  library(dplyr)
+  library(repr)
+  
+    borough_data <- reactive({
+      data<-data %>% filter(data$BORO %in% input$borough)
+    })
+  
   output$plot=renderPlot({
     
-    install.packages("shiny")
-    library(shiny)
+    par(mfrow = c(2, 5))
     
-    df=read.csv("C:/Users/naira_831868r/OneDrive/Desktop/Columbia/Spring '23/Applied Data Science/Assignments/Shiny App/DOHMH_New_York_City_Restaurant_Inspection_Results.csv")
-    head(df)
-    
-    #For wordclouds per violation code
-    df_violation_temp<-df |>
-      distinct(df$VIOLATION.CODE)
-    df_violation_temp
-    
-    freq=table(unlist(df$VIOLATION.CODE)) #Find frequency of violation codes to know what wordclouds make sense
-    freq 
-    
-    library(dplyr)
-    library(wordcloud)
+    data = borough_data()
+    ggplot(data, aes(x = factor(GRADE)))+  
+      geom_bar(width = 0.9) + coord_flip() +
+      scale_x_discrete(labels = function(x) str_wrap(x, width = 200))
     
     #Filtering
     
@@ -50,7 +77,7 @@ server <- function(input, output) {
       filter(VIOLATION.CODE=="02B")
     wc_data1 = 
       df1 |>
-      unnest_tokens(output = word, input = VIOLATION.DESCRIPTION, token='ngrams', n=2)|>
+      unnest_tokens(output = word, input = VIOLATION.DESCRIPTION)|>
       anti_join(y = stop_words)|>
       group_by(word)|>
       summarize(n = n())|>
@@ -62,7 +89,7 @@ server <- function(input, output) {
       filter(VIOLATION.CODE=="02G")
     wc_data2 = 
       df2 |>
-      unnest_tokens(output = word, input = VIOLATION.DESCRIPTION, token='ngrams', n=2)|>
+      unnest_tokens(output = word, input = VIOLATION.DESCRIPTION)|>
       anti_join(y = stop_words)|>
       group_by(word)|>
       summarize(n = n())|>
@@ -74,7 +101,7 @@ server <- function(input, output) {
       filter(VIOLATION.CODE=="04L")
     wc_data3 = 
       df3 |>
-      unnest_tokens(output = word, input = VIOLATION.DESCRIPTION, token='ngrams', n=2)|>
+      unnest_tokens(output = word, input = VIOLATION.DESCRIPTION)|>
       anti_join(y = stop_words)|>
       group_by(word)|>
       summarize(n = n())|>
@@ -86,7 +113,7 @@ server <- function(input, output) {
       filter(VIOLATION.CODE=="04N")
     wc_data4 = 
       df4 |>
-      unnest_tokens(output = word, input = VIOLATION.DESCRIPTION, token='ngrams', n=2)|>
+      unnest_tokens(output = word, input = VIOLATION.DESCRIPTION)|>
       anti_join(y = stop_words)|>
       group_by(word)|>
       summarize(n = n())|>
@@ -98,7 +125,7 @@ server <- function(input, output) {
       filter(VIOLATION.CODE=="06C")
     wc_data5 = 
       df5 |>
-      unnest_tokens(output = word, input = VIOLATION.DESCRIPTION, token='ngrams', n=2)|>
+      unnest_tokens(output = word, input = VIOLATION.DESCRIPTION)|>
       anti_join(y = stop_words)|>
       group_by(word)|>
       summarize(n = n())|>
@@ -110,7 +137,7 @@ server <- function(input, output) {
       filter(VIOLATION.CODE=="06D")
     wc_data6 = 
       df6 |>
-      unnest_tokens(output = word, input = VIOLATION.DESCRIPTION, token='ngrams', n=2)|>
+      unnest_tokens(output = word, input = VIOLATION.DESCRIPTION)|>
       anti_join(y = stop_words)|>
       group_by(word)|>
       summarize(n = n())|>
@@ -122,7 +149,7 @@ server <- function(input, output) {
       filter(VIOLATION.CODE=="08A")
     wc_data7 = 
       df7 |>
-      unnest_tokens(output = word, input = VIOLATION.DESCRIPTION, token='ngrams', n=2)|>
+      unnest_tokens(output = word, input = VIOLATION.DESCRIPTION)|>
       anti_join(y = stop_words)|>
       group_by(word)|>
       summarize(n = n())|>
@@ -134,7 +161,7 @@ server <- function(input, output) {
       filter(VIOLATION.CODE=="10B")
     wc_data8 = 
       df8 |>
-      unnest_tokens(output = word, input = VIOLATION.DESCRIPTION, token='ngrams', n=2)|>
+      unnest_tokens(output = word, input = VIOLATION.DESCRIPTION)|>
       anti_join(y = stop_words)|>
       group_by(word)|>
       summarize(n = n())|>
@@ -146,14 +173,13 @@ server <- function(input, output) {
       filter(VIOLATION.CODE=="10F")
     wc_data9 = 
       df9 |>
-      unnest_tokens(output = word, input = VIOLATION.DESCRIPTION, token='ngrams', n=2)|>
+      unnest_tokens(output = word, input = VIOLATION.DESCRIPTION)|>
       anti_join(y = stop_words)|>
       group_by(word)|>
       summarize(n = n())|>
       ungroup()|>
       arrange(desc(n))
-    wordcloud(words = wc_data9$word, freq = wc_data9$n,scale = c(2,0.5),max.words = 200,rot.per = 0)
-    
+    wordcloud(words = wc_data9$word, freq = wc_data9$n,scale = c(2,0.5),max.words = 200,rot.per = 0)    
   })
 }  
 # Run the application 
